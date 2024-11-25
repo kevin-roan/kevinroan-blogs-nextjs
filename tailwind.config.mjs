2861e3c65f67a3a33a0512260931f841efba0689
Kevin Roan <kevinroan@proton.me>
2024-11-25 11:01:49 +0530

    feat: view markdown

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"M PLUS Rounded 1c"', "sans-serif"], // Adding the Google Font to Tailwind's sans family
+        sans: ['"M PLUS Rounded 1c"', "sans-serif"],
+        libre: ['"Libre Baskerville"', "sans-serif"],
       },
       boxShadow: {
         custom: "0px 4px 4px 0px rgba(159, 159, 159, 0.10)",
